9ad465332aff0c1e9dd426c18a1b34e4d079d7fd
M0mahmoud <houdmohamed85@gmail.com>
2022-08-21 16:18:46 +0200

    Adding Notes App

---
+++ stacks/ApiStack.js
@@ -0,0 +1,33 @@
+import { Api, use } from "@serverless-stack/resources";
+import { StorageStack } from "./StorageStack";
+
+export function ApiStack({ stack, app }) {
+  const { table } = use(StorageStack);
+
+  // Create the API
+  const api = new Api(stack, "Api", {
+    defaults: {
+      authorizer: "iam",
+      function: {
+        permissions: [table],
+        environment: {
+          TABLE_NAME: table.tableName,
+          STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+        },
+      },
+    },
+    routes: {
+      "POST /billing": "functions/billing.main",
+    },
+  });
+
+  // Show the API endpoint in the output
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+
+  // Return the API resource
+  return {
+    api,
+  };
+}
